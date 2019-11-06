2a90f97f0bc23a6602e9b0920da4b697a7e633ae
Emily Turner (RIT Student) <ekt6170@ad.rit.edu>
2019-11-06 18:47:42 -0500

    Level Select Images

--- src/js/level-select.js
+++ src/js/level-select.js
@@ -1,1 +1,23 @@
 import '../scss/style.scss';
+
+function toggleProfile() {
+    let navBar = document.querySelector('nav');
+    let profile = document.querySelector('#profile');
+    let container = document.querySelector('#levelContainer');
+    if(profile.style.width == "0px" || profile.style.width == ""){
+        profile.style.width = "300px";
+        container.style.marginLeft = "300px";
+        container.style.transition = "0.2s";
+        navBar.style.width = "81vw";
+        navBar.style.left = "350px";
+        container.style.borderColor = "#33ff00";
+        
+    } else{
+        profile.style.width = "0px";
+        container.style.marginLeft = "0px";
+        container.style.transition = "0.3s";
+        navBar.style.width = "100vw";
+        navBar.style.left = "0px";
+        container.style.borderColor = "#111";
+    }
+}
