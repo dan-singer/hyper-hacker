4d6ccce15c521fe0184210bee9058888bc3794de
Dan Singer (RIT Student) <djs5435@ad.rit.edu>
2019-11-06 18:38:05 -0500

    Add model functionality to handle completing the tutorial

--- server/router.js
+++ server/router.js
@@ -3,7 +3,8 @@ const mid = require('./middleware');
 const router = (app) => {
   app.get('/login', mid.requiresSecure, mid.bypassIfLoggedIn, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.bypassIfLoggedIn, controllers.Account.login);
-  app.get('/tutorial', mid.requiresSecure, mid.bypassIfLoggedIn, controllers.Account.tutorialPage);
+  app.get('/tutorial', mid.requiresSecure, mid.redirectHomeIfLoggedOut, controllers.Account.tutorialPage);
+  app.post('/tutorial', mid.requiresSecure, mid.redirectHomeIfLoggedOut, controllers.Account.completeTutorial);
   app.get('/signup', mid.requiresSecure, mid.bypassIfLoggedIn, controllers.Account.signupPage);
   app.post('/signup', mid.requiresSecure, mid.bypassIfLoggedIn, controllers.Account.signup);
   app.get('/logout', mid.redirectHomeIfLoggedOut, controllers.Account.logout);
