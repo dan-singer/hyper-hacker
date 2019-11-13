c0a4dc7994428d3dd21d2fb95f753fd72015157e
Emily Turner (RIT Student) <ekt6170@ad.rit.edu>
2019-11-13 14:49:54 -0500

    Continued flocking work

--- src/js/levels/5.js
+++ src/js/levels/5.js
@@ -2,7 +2,19 @@ import '../../scss/style.scss';
 import '../../scss/levels.scss';
 import {toggleProfile} from '../support/utils.js'
 
+document.onmousemove = outputMouse;
 
+function outputMouse(event){
+    let x = event.clientX;
+    let y = event.clientY;
+
+    for(let i=0; i<boidArray.length; i++){
+        boidArray[i].target.x = x;
+        boidArray[i].target.y = y;
+    }
+}
+
+let boidArray = []; //array of beautiful boids
 
 function init() {
     document.querySelector('#profile-toggle').onclick = toggleProfile;
@@ -26,15 +38,15 @@ function init() {
             });
         };
     }
+
+    makeFlock();
 }
 
 function makeBoids(){
-    for(let i=0; i<50; i++){
+    for(let i=0; i<boidArray.length; i++){
         let boid = document.createElement('div');
-        let x = Math.floor(Math.random() * 200)-100;
-        let y = Math.floor(Math.random() * 200)-100;
-        boid.style.top = y +'px';
-        boid.style.left= x + 'px';
+        boid.style.top = boidArray[i].position.y +'px';
+        boid.style.left= boidArray[i].position.x + 'px';
         boid.setAttribute("class", "boid");
         let content = document.createElement('p');
         content.innerHTML= "CLICK ME";
@@ -43,11 +55,24 @@ function makeBoids(){
     }
 }
 
+function makeFlock(){
+
+    for(let i=0; i< 100; i++){
+        let newBoid = new Boid();
+        boidArray.push(newBoid);
+    }
+
+    console.log(boidArray);
+
+    makeBoids();
+}
+
+//credit to past Emily Turner for doing her IMD project good
 class Boid{
     constructor(){ //sets random x and y coordinates from -100 to 100
-        this.x = Math.floor(Math.random() * 200)-100;
-        this.y = Math.floor(Math.random() * 200)-100;
-        this.position = {x: this.x, y:this.y};
+        this.x = Math.floor(Math.random() * screen.width)-50; //x position of the Boid
+        this.y = Math.floor(Math.random() * screen.height)-50; //y position of the Boid
+        this.position = {x: this.x, y:this.y}; //position of the Boid
         this.direction = {x:0, y:0};
         this.velocity = {x:0, y:0};
         this.acceleration = {x:0, y:0};
@@ -55,7 +80,7 @@ class Boid{
         this.maxForce = 10;
         this.centroid = {x: 0, y:0};
         this.alignment = {x:0, y:0};
-        this.target = {x:0, y:0};
+        this.target = {x:(screen.width/2), y:(screen.height/2)};
     }
 
     ApplyForce(force){
@@ -72,11 +97,19 @@ class Boid{
         return c;
     }
 
+    Normalize(vec2){
+        let newVec = {x:0, y:0};
+        newVec.x = vec2.x / this.CalcMagnitude({x:0, y:0}, vec2);
+        newVec.y = vec2.y / this.CalcMagnitude({x:0, y:0}, vec2);
+        
+        return newVec;
+    }
+
+    //returns seeking force towards a given target vector
     Seek(aim){
         let desiredVelocity = {x:aim.x-this.position.x, y:aim.y-this.position.y};
 
-        desiredVelocity.x = desiredVelocity.x / this.CalcMagnitude({x:0, y:0}, desiredVelocity);
-        desiredVelocity.y = desiredVelocity.y / this.CalcMagnitude({x:0, y:0}, desiredVelocity);
+        desiredVelocity = this.Normalize(desiredVelocity);
 
         desiredVelocity.x *= this.maxSpeed;
         desiredVelocity.y *= this.maxSpeed;
@@ -113,6 +146,18 @@ class Boid{
     }
 
     UpdatePosition(){
+        //adds acceleration to velocity
+        this.velocity += this.acceleration;
+
+        //adds velocity to position
+        this.position += this.velocity;
+
+        //normalizes the direction
+        this.direction = this.Normalize(this.velocity);
+
+        //reseta acceleration to zero for the next frame
+        this.acceleration.x = 0;
+        this.acceleration.y = 0;
 
     }
 
@@ -121,3 +166,5 @@ class Boid{
         UpdatePosition();
     }
 }
+
+window.onload = init;
