0765dac8c3f0f4ce3ae44ef7150438045635a08d
Dan <dsinger.sax@gmail.com>
2019-11-14 15:26:09 -0500

    Implement level 1 and add level names to data file

--- src/js/levels/1.js
+++ src/js/levels/1.js
@@ -1,16 +1,24 @@
 import "../../scss/style.scss";
-import "../../scss/level1.scss";
+import "../../scss/levels.scss";
 const THREE = require('three');
 const ObjLoader = require('three-obj-loader');
 
 let scene, camera, renderer, cube;
+let levelNum, csrf;
 
 const main = () => {
+    console.log('Welcome to Level 1! You are in the right place! Start typing \"set\" and see what options are available to you...');
+
+    const urlParams = new URLSearchParams(window.location.search);
+    levelNum = parseInt(urlParams.get('num'));
+    csrf = document.querySelector('#_csrf').value;
+
+
     const canvas = document.querySelector('#three-canvas');
     renderer = new THREE.WebGLRenderer({canvas});
     renderer.setClearColor(0x111111);
 
-    camera = new THREE.PerspectiveCamera(75, 640/480, 0.1, 5);
+    camera = new THREE.PerspectiveCamera(75, 640/480, 0.1, 10);
     camera.position.z = 2;
 
     scene = new THREE.Scene();
@@ -30,22 +38,52 @@ const main = () => {
         'assets/media/arrow.obj',
         (obj) => {            
             obj.scale.set(0.1,0.1,0.1);
-            const arrowsPerRing = 50;
+            const arrowsPerRing = 15;
+            const rings = 20;
             const radius = 5;
-            for (let i = 0; i < arrowsPerRing; ++i) {
-                let angle = Math.PI * 2 * (i / arrowsPerRing);
-                let x = Math.cos(angle) * radius;
-                let z = Math.sin(angle) * radius;
-                let pos = new THREE.Vector3(x, 0, z);
-                let clone = obj.clone();
-                clone.position.set(pos);
-                clone.lookAt(cube.position);
-
-                scene.add(clone); 
+            for (let i = 0; i < rings; ++i) {
+                let axis = new THREE.Vector3(radius, 0, 0);
+                let axisPitch = Math.PI * 2 * (i / rings);
+                axis.applyEuler(new THREE.Euler(0, 0, axisPitch));
+                for (let j = 0; j < arrowsPerRing; ++j) {
+                    let axisYaw = Math.PI * 2 / arrowsPerRing;
+                    axis.applyEuler(new THREE.Euler(0, axisYaw, 0));
+                    let clone = obj.clone();
+                    clone.position.set(axis.x, axis.y, axis.z);
+                    clone.lookAt(cube.position);
+                    scene.add(clone); 
+                }
             }
+
         }
     );
 
+
+    canvas.onclick = e => {
+        // @see https://discourse.threejs.org/t/click-event-on-object/1320/2
+        // @see https://www.pericror.com/software/creating-3d-objects-with-click-handlers-using-three-js/
+        let raycaster = new THREE.Raycaster();
+        let mouse = {
+            x: (e.layerX / canvas.clientWidth) * 2 - 1,
+            y: -(e.layerY / canvas.clientHeight) * 2 + 1
+        }
+        raycaster.setFromCamera(mouse, camera);
+        let intersects = raycaster.intersectObject(cube, true);
+        if (intersects.length > 0) {
+
+            fetch(`/level?num=${levelNum}&_csrf=${csrf}`, {
+                method: 'POST',
+            })
+            .then(res => {
+                if (res.status === 200) {
+                    window.location.href = '/level-select';
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    }
 
 
     const dirLight = new THREE.DirectionalLight("white", 1.0);
@@ -55,10 +93,15 @@ const main = () => {
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 
-    // Bind variables to window so hackers can access them
-    window.camera = camera;
-
 };
+
+window.setPitch = (pitch) => {
+    camera.rotation.x = pitch;
+}
+
+window.setYaw = (yaw) => {
+    camera.rotation.y = yaw;
+}
 
 const render = (time) => {
     renderer.render(scene, camera);
