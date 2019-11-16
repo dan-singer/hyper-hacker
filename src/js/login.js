7f0d78692366714033cd2dc92cd8ded072a192e6
Dan <dsinger.sax@gmail.com>
2019-11-15 21:31:49 -0500

    Implement ajax login and signup pages

--- src/js/login.js
+++ src/js/login.js
@@ -1,1 +1,36 @@
 import '../scss/style.scss';
+import Swal from "sweetalert2";
+const serialize = require('form-serialize');
+
+const init = () => {
+    const form = document.querySelector('#loginForm');
+    form.onsubmit = e => {
+        e.preventDefault();
+        const body = serialize(form);
+        fetch(`/login`, {
+            method: 'POST',
+            body,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then(res => {
+            if (!res.ok) {
+                return res.json()
+            }
+            else {
+                window.location.href = '/level-select';
+            }
+        })
+        .then(data => {
+            if (data.error) {
+                Swal.fire(`Error: ${data.error}`);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    };
+}
+
+window.onload = init;
